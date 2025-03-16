8bb00ab35b259670647c961caa8242a56c237ec2
ahmed osama <ahmedosama21049@gmail.com>
2025-03-17 01:40:37 +0200

    feat(db): add check_DB_exists function to verify database existence

--- DatabaseScripts/Create_DB.sh
+++ DatabaseScripts/Create_DB.sh
@@ -3,4 +3,34 @@
 # Source the helper file
 source ../helper.sh
 
+# The create_DB function is used to create a new database. 
+function create_DB {
+    dbName=$(zenity --entry --title="Create DB" --text="Enter the database name:")
+    if [ $? -ne 0 ]; then
+        source "$Main_Menu_Script"
+        return  
+    fi
+    if [ -z "$dbName" ]; then
+        zenity --error --text="No name entered. Exiting..."
+        create_DB
+    fi
+    dbName=$(echo "$dbName" | xargs | awk '{print tolower($0)}' | sed 's/ /_/g')
+    validate_name "$dbName"
+    if [ $? -ne 0 ]; then
+        zenity --error --text="Invalid name $dbName. Exiting..."
+        create_DB
+    fi
+    check_DB_exists "$dbName"
+    if [ $? -ne 0 ]; then
+        zenity --error --text="Database already exists. Exiting..."
+        create_DB
+        return
+    else
+        mkdir -p "$dbPath/$dbName"
+        zenity --info --text="Database created successfully "
+        # source "$create_Table_Script"
+        return
+    fi
+}
+
 create_DB
--- helper.sh
+++ helper.sh
@@ -9,36 +9,22 @@ create_Table_Script="./create_Table.sh"
 Main_Menu_Script="./Main_Menu.sh"
 
 
-
-# The create_DB function is used to create a new database. 
-function create_DB {
-    dbName=$(zenity --entry --title="Create DB" --text="Enter the database name:")
-    if [ $? -ne 0 ]; then
-        source "$Main_Menu_Script"
-        return  
-    fi
-    if [ -z "$dbName" ]; then
-        zenity --error --text="No name entered. Exiting..."
-        create_DB
-    fi
-    dbName=$(echo "$dbName" | xargs | awk '{print tolower($0)}' | sed 's/ /_/g')
-    validate_name "$dbName"
-    if [ -d "$dbPath/$dbName" ]; then
-        zenity --error --text="Database already exists. Exiting..."
-        create_DB
-    else
-        mkdir -p "$dbPath/$dbName"
-        zenity --info --text="Database created successfully "
-        source "$create_Table_Script"
-        return
-    fi
-}
-
 # The validate_name function is used to validate the database name entered by the user.
 function validate_name {
-    dbName=$1
-    if [[ ! "$dbName" =~ ^[a-zA-Z][a-zA-Z0-9_]*$ ]]; then
-        zenity --error --text="Invalid database name. Must start with a letter and contain only letters, numbers, and underscores."
-        create_DB  
+    name=$1
+    if [[ ! "$name" =~ ^[a-zA-Z][a-zA-Z0-9_]*$ ]]; then
+        return 1
     fi
+    return 0
+}
+
+
+# The check_DB_exists function is used to check if the database already exists.
+function check_DB_exists {
+    dbName=$1
+    if [ -d "$dbPath/$dbName" ]; then
+        return 1
+    else
+        return 0
+    fi    
 }

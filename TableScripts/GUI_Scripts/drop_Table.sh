69e97e8470470c9fadce6fca3725e95ec65dc6ca
ahmed osama <ahmedosama21049@gmail.com>
2025-03-21 19:11:02 +0200

    feat(db): add script for dropping tables

---
+++ TableScripts/GUI_Scripts/drop_Table.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+source ../../helper.sh
+Database_Scripts_Path="../../DatabaseScripts"
+source $Database_Scripts_Path/DB_Menu.sh 
+
+echo "from drop_Table $(pwd)"   
+
+# The drop_Tables function is used to delete a table from the selected database.
+function drop_Tables {
+
+    table_name="$1"
+
+    if [[ -z "$table_name" ]]; then
+        zenity --error --text="Error: No table name provided!"
+        Table_Menu "../"
+        exit 1
+    fi
+
+    db_name=$(basename "$(pwd)")
+
+    echo "tables: $(ls)"
+    rm -r "$table_name"
+
+    zenity --info --text="Table '$table_name' has been dropped successfully."
+
+    Table_Menu "$db_name"
+}
+
+drop_Tables "$1"

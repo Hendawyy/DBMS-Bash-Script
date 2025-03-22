8438fe852a8aebcd474c90570edda83d8e5dd088
ahmed osama <ahmedosama21049@gmail.com>
2025-03-22 19:32:15 +0200

    refactor(db): improve code reusability in DeleteFrom Table

--- TableScripts/GUI_Scripts/delete_from_table.sh
+++ TableScripts/GUI_Scripts/delete_from_table.sh
@@ -1,42 +1,5 @@
 #!/bin/bash
-
-function filter_matching() {
-    tableDataPath="$1"
-    columnNumberZ="$2"
-    operator="$3"
-    value="$4"
-    tempFile="$5"
-    functionName="$6"
-    
-    awk -F ":" -v col="$columnNumberZ" -v val="$value" -v op="$operator" -v func="$functionName" '
-        BEGIN { IGNORECASE=1; }
-        {
-            columnValue = $col
-            compareValue = val
-            isNumeric = (columnValue + 0 == columnValue) && (compareValue + 0 == compareValue)
-
-            if (isNumeric) {
-                columnValue += 0
-                compareValue += 0
-            }
-
-            matchFound = 0
-
-            if (op == "==" && columnValue == compareValue) matchFound = 1
-            if (op == "!=" && columnValue != compareValue) matchFound = 1
-            if (op == ">=" && columnValue >= compareValue) matchFound = 1
-            if (op == "<=" && columnValue <= compareValue) matchFound = 1
-            if (op == ">"  && columnValue > compareValue)  matchFound = 1
-            if (op == "<"  && columnValue < compareValue)  matchFound = 1
-
-            if (func == "Delete") {
-                if (!matchFound) { print $0 }
-            } else {
-                if (matchFound) { print $0 }
-            }
-
-        }' "$tableDataPath" > "$tempFile"
-}
+source ../../helper.sh
 
 function Filter_And_Delete_Rows() {
     tableName=$1
@@ -81,28 +44,6 @@ function Filter_And_Delete_Rows() {
     Column_Number_filter=$(awk -F: -v selected_col="$ColumnCondition" 'NR>3 && $1 == selected_col {print NR-3}' "$tableName/$tableName.md")
 
     Filter_AND_Delete "$tableName" "$tableName/$tableName.md" "$Column_Number_filter" "$Selected_Operator" "$Condition_Value"
-}
-
-function Filter_AND_Delete() {
-    tableName=$1
-    metaDataPath=$2
-    columnNumber=$3
-    operator=$4
-    value=$5
-    dbName=$(basename "$(pwd)")
-
-    functionName="Delete"
-
-    tempFile=$(mktemp)
-    filter_matching "$tableName/$tableName" "$columnNumber" "$operator" "$value" "$tempFile" "$functionName"
-    if [ $(wc -l < "$tempFile") -eq $(wc -l < "$tableName/$tableName") ]; then
-        zenity --error --text="No Rows Found Matching the Condition"
-    else
-        mv "$tempFile" "$tableDataPath"
-        zenity --info --text="Rows Matching the Condition have been Deleted Successfully"
-    fi
-
-    Table_Menu $dbName
 }
 
 Filter_And_Delete_Rows "$1"
--- helper.sh
+++ helper.sh
@@ -330,5 +330,63 @@ check_not_null() {
     return 0  
 }
 
+# The filter_matching function is used to filter rows based on a condition.
+function filter_matching() {
+    tableDataPath="$1"
+    columnNumberZ="$2"
+    operator="$3"
+    value="$4"
+    tempFile="$5"
+    functionName="$6"
+    
+    awk -F ":" -v col="$columnNumberZ" -v val="$value" -v op="$operator" -v func="$functionName" '
+        BEGIN { IGNORECASE=1; }
+        {
+            columnValue = $col
+            compareValue = val
+            isNumeric = (columnValue + 0 == columnValue) && (compareValue + 0 == compareValue)
 
+            if (isNumeric) {
+                columnValue += 0
+                compareValue += 0
+            }
 
+            matchFound = 0
+
+            if (op == "==" && columnValue == compareValue) matchFound = 1
+            if (op == "!=" && columnValue != compareValue) matchFound = 1
+            if (op == ">=" && columnValue >= compareValue) matchFound = 1
+            if (op == "<=" && columnValue <= compareValue) matchFound = 1
+            if (op == ">"  && columnValue > compareValue)  matchFound = 1
+            if (op == "<"  && columnValue < compareValue)  matchFound = 1
+
+            if (func == "Delete") {
+                if (!matchFound) { print $0 }
+            } else {
+                if (matchFound) { print $0 }
+            }
+
+        }' "$tableDataPath" > "$tempFile"
+}
+
+function Filter_AND_Delete() {
+    tableName=$1
+    metaDataPath=$2
+    columnNumber=$3
+    operator=$4
+    value=$5
+    dbName=$(basename "$(pwd)")
+
+    functionName="Delete"
+
+    tempFile=$(mktemp)
+    filter_matching "$tableName/$tableName" "$columnNumber" "$operator" "$value" "$tempFile" "$functionName"
+    if [ $(wc -l < "$tempFile") -eq $(wc -l < "$tableName/$tableName") ]; then
+        zenity --error --text="No Rows Found Matching the Condition"
+    else
+        mv "$tempFile" "$tableDataPath"
+        zenity --info --text="Rows Matching the Condition have been Deleted Successfully"
+    fi
+
+    Table_Menu $dbName
+}
